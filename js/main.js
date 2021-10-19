2a7c05dc472b53f84a8f4d64d23266b3cae144fb
DongrockOh <87560481+DongrockOh@users.noreply.github.com>
2021-10-19 14:41:37 +0900

    #2 헤더와 드롭다운 메뉴-2
    1. 메인 메뉴
    2. BEM
    3. 전역배지(lodash, GSAP)

--- js/main.js
+++ js/main.js
@@ -14,3 +14,28 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+// badge 스크롤 이벤트 및 애니메이션
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      // 배지 숨기기
+      // gsap.to(요소, 지속시간, 옵션);
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      // 배지 보이기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
+// _.throttle(함수, 시간(ms))
