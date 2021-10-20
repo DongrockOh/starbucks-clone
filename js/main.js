c9dc82b55d462cef4815a2f1d9d8e254752fdf11
DongrockOh <87560481+DongrockOh@users.noreply.github.com>
2021-10-20 15:01:00 +0900

    #4 오타 수정
    에러 해결

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ new Swiper(".notice-line .swiper-container", {
 });
 
 new Swiper(".promotion .swiper-container", {
-  slidePerView: 3, // 한번에 보여줄 슬라이드 개수
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
   spaceBetween: 10, // 슬라이드 사이 여백
   centeredSlides: true, // 1번 슬라이드가 가운데 보이기
   loop: true,
