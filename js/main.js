cb8d63b9a426bc0ba04911685b2d41e9da29f3ca
DongrockOh <87560481+DongrockOh@users.noreply.github.com>
2021-10-21 14:14:50 +0900

    #5 유튜브 영상
    1. 영상배경 - rewards
    2. Youtube iframe API
    3. 반복 애니메이션

--- js/main.js
+++ js/main.js
@@ -21,7 +21,6 @@ const badgeEl = document.querySelector("header .badges");
 window.addEventListener(
   "scroll",
   _.throttle(function () {
-    console.log(window.scrollY);
     if (window.scrollY > 500) {
       // 배지 숨기기
       // gsap.to(요소, 지속시간, 옵션);
@@ -87,3 +86,29 @@ promotionToggleBtn.addEventListener("click", function () {
     promotionEl.classList.remove("hide");
   }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 '문자 데이터'를,
+  // `parseFloat()`을 통해 소수점을 가지는 '숫자 데이터'로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(
+    selector, //선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    {
+      // 옵션
+      y: 20,
+      repeat: -1,
+      yoyo: true,
+      ease: Power1.easeInOut,
+      delay: random(0, delay),
+    }
+  );
+}
+floatingObject(".floating1", 1, 15);
+floatingObject(".floating2", 0.5, 15);
+floatingObject(".floating3", 1.5, 20);
