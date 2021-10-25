1f7819fefb03acf37800d8047b81279255a57c68
DongrockOh <87560481+DongrockOh@users.noreply.github.com>
2021-10-25 13:24:48 +0900

    #11 페이지 상단으로 이동
    gsap 이용 - scrollTo

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ searchInputEl.addEventListener("blur", function () {
 
 // badge 스크롤 이벤트 및 애니메이션
 const badgeEl = document.querySelector("header .badges");
+const toTopEl = document.querySelector("#to-top");
 
 window.addEventListener(
   "scroll",
@@ -28,16 +29,30 @@ window.addEventListener(
         opacity: 0,
         display: "none",
       });
+      // to-top 버튼 보이기
+      gsap.to(toTopEl, 0.2, {
+        x: 0,
+      });
     } else {
       // 배지 보이기
       gsap.to(badgeEl, 0.6, {
         opacity: 1,
         display: "block",
       });
+      // to-top 버튼 숨기기
+      gsap.to(toTopEl, 0.2, {
+        x: 100,
+      });
     }
   }, 300)
 );
 // _.throttle(함수, 시간(ms))
+
+toTopEl.addEventListener("click", function () {
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
 
 const fadeEls = document.querySelectorAll(".visual .fade-in");
 fadeEls.forEach(function (fadeEl, index) {
